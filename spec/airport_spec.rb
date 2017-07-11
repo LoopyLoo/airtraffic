83abcd86df2fd35503fbdc41f85cb8f4b3beeeac
Peter Apostolou <p.p.a@btinternet.com>
2017-07-11 12:01:13 +0100

    third change

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,9 +3,14 @@ require 'airport'
 describe 'myairport methods' do 
 	
 
+		context 'Plane parked' do
+		it'adds to the airport list of landed planes' do
+		expect(add_to_airport_list).to eq 'big plane'
+		end
+	end
+
 	context 'Instructing plane to land' do
 		it 'lands the plane' do
-		
 		expect(instruct_to_land).to eq 'Landed'
 		end
 	end
@@ -20,6 +25,16 @@ describe 'myairport methods' do
 		it 'stops take off' do
 			expect(prevent_takeoff).to eq 'take off not allowed'
 		end
+
+		it 'checks the weather' do
+			weather="bad man"
+			expect(todays_weather).to eq 'bad man'
+		end
+
 	end
+
+
+
+
  end
 
